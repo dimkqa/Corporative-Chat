64aae1ddb296dc9012934e8bd45e9da80c1f80b6
Shamrik <Shamrik@ckba.com>
2019-03-10 18:01:27 +0600

    Added user class and user manager

---
+++ Console-Server/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Server
+{
+    public class User
+    {
+        /// <summary>
+        ///     Имя пользователя
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        ///     Логин пользователя
+        /// </summary>
+        public string Login { get; set; }
+
+        /// <summary>
+        ///     Зашифрованный пароль пользователя
+        /// </summary>
+        public string Password { get; set; }
+    }
+}
---
+++ Console-Server/UserManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Server
+{
+    public class UserManager
+    {
+        public UserManager(User user)
+        {
+
+        }
+        
+        public void UserDuplicateValidation()
+        {
+
+        }
+
+        public void SignUp()
+        {
+
+        }
+
+        public void SingnIn()
+        {
+
+        }
+    }
+}
